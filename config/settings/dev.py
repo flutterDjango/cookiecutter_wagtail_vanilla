65017660992d4605017ea78e6b50f003c838a786
unknown <flutterdjangodev@gmail.com>
2024-06-18 20:33:46 +0200

    first commit

---
+++ config/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-@pk6n$(lntig0!#4a3or4%2(#y)l3t*jn39vs_=2f6*^@hidpo"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
